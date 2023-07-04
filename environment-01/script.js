79c00065b8245e449cf330d66a895f23dd2e6242
Adelkilde <William.adelkilde@gmail.com>
2023-07-04 17:31:43 +0200

    👍👍

--- environment-01/script.js
+++ environment-01/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+let students = [];
+
+async function initApp() {
+  console.log("JS is running");
+  await getStudents();
+  console.log(students);
+  showStudents();
+}
+
+async function getStudents() {
+  const response = await fetch("students.json");
+  const data = await response.json();
+  students = data;
+}
+
+function showStudents() {
+  document.querySelector("#students-list").innerHTML = "";
+
+  sortBySemester();
+  for (const student of students) {
+    const html = /*html*/ `
+    <li>${student.name} - ${student.mail}, semester: ${student.semester} <btn>👍</btn></li>`;
+    document.querySelector("#students-list").insertAdjacentHTML("beforeend", html);
+    document.querySelector("#students-list li:last-child btn");
+  }
+}
+
+function sortBySemester() {
+  students.sort((a, b) => a.semester - b.semester);
+}
